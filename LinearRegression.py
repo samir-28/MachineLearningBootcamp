872770ea9620a71de73aca5228901d4b4c3714d2
Sawmeer Bazgain <samirbajgain9@gmail.com>
2024-08-28 10:43:14 +0545

    SkLearnLibraryUse

--- LinearRegression.py
+++ LinearRegression.py
@@ -25,13 +25,17 @@ b0 = np.mean(y) - b1 * np.mean(x)
 print(f'Calculated coefficients: b0 = {b0}, b1 = {b1}')
 print(f'Equation of the line: Y = {b0} + {b1}X')
 
-# Loss function: Mean Squared Error (MSE)
+# Loss function: Mean Squared Error (MSE)  
 def calculate_loss(x, y, b0, b1):
-    y_pred = regression(x, b0, b1)  # Predictions based on the model
+    y_pred = regression(x, b0, b1) 
+    print(y_pred)# Predictions based on the model
     mse = np.mean((y_pred - y) ** 2)  # Mean Squared Error (MSE)
     return mse
 
-# User input for prediction
+# Gradient descent => finnding minimum loss
+# Rule to update: W - (partial derivative) dl /  (partial derivative) dw
+
+# User input for predicion                          
 a = int(input("Enter the value of X: "))
 predicted_y = regression(a, b0, b1)
 print(f'When X = {a}, Y = {predicted_y}')
